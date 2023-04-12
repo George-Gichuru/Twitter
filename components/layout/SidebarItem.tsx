747592635a67710a566b1541a1adb6b8c1814d4c
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2023-04-12 13:32:56 +0300

    css

--- components/layout/SidebarItem.tsx
+++ components/layout/SidebarItem.tsx
@@ -34,6 +34,25 @@ const SidebarItem: React.FC<SidebarItemProps> = ({
 
             </div>
 
+            <div className="
+                relative
+                hidden
+                lg:flex
+                items-center
+                gap-4
+                p-4
+                rounded-full
+                hover:bg-slate-300
+                hover:bg-opacity-10
+                cursor-pointer">
+
+                    <Icon size={28} color="white" />
+                    <p className="hidden lg:block text-white text-xl">
+                        {label}
+                    </p>
+
+            </div>
+
         </div>
     )
 }
