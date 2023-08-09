913ba72a38eae5703df44ac5dc41a2c69931f4f8
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2023-08-09 14:02:20 +0300

    X

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,13 @@ import RegisterModal from '../components/modals/RegisterModal'
 import '../styles/globals.css'
 import EditModal from '../components/modals/EditModal';
 
-import Head from "next/head"
+import Head from "next/head";
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "X twitter",
+  description: "Welcome to X"
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
